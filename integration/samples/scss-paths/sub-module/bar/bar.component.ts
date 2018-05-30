d6e1ec83712a48dd643d4a3aba57ddba0577df7e
Georgios Kaleadis <georgios.kaleadis@sinnerschrader.com>
2018-05-30 16:19:08 +0200

    scss import paths and ngc error

--- integration/samples/scss-paths/sub-module/bar/bar.component.ts
+++ integration/samples/scss-paths/sub-module/bar/bar.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { someOtherFoo } from './foo';
 
 @Component({
   selector: 'bar-component',
   templateUrl: './bar.component.html',
   styleUrls: ['./bar.component.scss', './bar.component.less', './bar.component.styl']
 })
-export class BarComponent {}
+export class BarComponent {
+  constructor() {
+    console.log(someOtherFoo('abc'));
+  }
+}
